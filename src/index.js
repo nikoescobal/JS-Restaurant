b751a0562922626e85a8e1d3ab84dcc71ff7e5da
Niko Escobal <niko.escobal@gmail.com>
2021-08-19 21:31:44 +0800

    Add heart to footer

--- src/index.js
+++ src/index.js
@@ -29,11 +29,12 @@ content.appendChild(mainText);
 const footer = document.createElement('footer');
 
 footer.innerHTML = `
-  <footer class="px-4 py-6 flex items-center absolute bottom-0 justify-center text-blue-500 w-full bg-gray-100 font-mono italic z-50">
+  <footer class="px-4 py-6 flex items-center absolute bottom-0 justify-center text-blue-500 w-full bg-gray-100 font-sans italic z-50">
     <p class="flex space-x-2">
     <p> Copyright &copy; 2020-2021.&nbsp </p>
-    <p> Made by Shiver Tech</p>
-    <a href="logo-1.svg"></a>
+    <p> Made by Shiver Tech&nbsp</p>
+    <div class='w-3'><img class='w-full h-full' src="./assets/logo-1.svg" alt="heart image"></div>
+    
   </footer>
 `
 content.appendChild(footer);
